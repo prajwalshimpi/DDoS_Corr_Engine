990c0905a1fbef4dffb91cd878410f3daae48d0f
ozch <osamac@outlook.com>
2018-06-29 22:24:56 +0500

    added cvs mysql db file and made a few changes

--- DataFeed.py
+++ DataFeed.py
@@ -13,7 +13,7 @@ cursor = connection.cursor()
 cursor.execute("select DISTINCT ts from corr where pk>16")
 
 # fetch all of the rows from the query
-data = cursor.fetchmany(100)
+data = cursor.fetchall()
 
 # print the rows
 if data == 0:
@@ -44,7 +44,10 @@ for time in data:
                 dict.setdefault(str(le), temp)
                 le += 1
     for n in range(0, le):
-        print(" \t " + str(dict[str(n)]))
+        if dict[str(n)]['T'] > 15 :
+            print(" \t " + str(dict[str(n)]))
+        else:
+            print(" \t No Sign of Attack")
     dict = {}
 # close the cursor object
 cursor.close()
