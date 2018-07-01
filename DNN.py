7ca260280dd8f280dd5940bf132fccb2606a710e
ozch <osamac@outlook.com>
2018-07-01 21:59:14 +0500

    write the DataPipeline.py:(to Feed data to NN) Class and DNN.py(Main Class with algorithm) there is an error in DNN.py

---
+++ DNN.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import, division, print_function
+import itertools
+import pandas as pd
+import os
+import tensorflow as tf
+DIR = "./MODEL"
+from DataPipeline import *
+#tf.enable_eager_execution()
+print("TensorFlow version: {}".format(tf.VERSION))
+#print("Eager execution: {}".format(tf.executing_eagerly()))
+pipe = DataPipeline()
+(train_feature, train_label), (test_feature, test_label) = pipe.load_data()
+sess = tf.Session()
+
+#Features ['ip','port','loc_country','zip','protocol','times']
+loc_cul=tf.feature_column.categorical_column_with_vocabulary_list("loc_country", ["HU", "US", "PK", "RU", "MY", "SE", "KR"])
+pro_cul=tf.feature_column.categorical_column_with_vocabulary_list("protocol", ["HTTP","HTTPS", "ICMP", "UDP","TCP"])
+h_b_size = 100
+e_c_size = h_b_size**0.25
+ip_cul=tf.feature_column.categorical_column_with_hash_bucket("ip",h_b_size),
+
+
+my_feature_columns = [
+    #tf.feature_column.embedding_column(categorical_column=ip_cul,dimension=e_c_size),
+    #tf.feature_column.embedding_column(categorical_column=ip_cul, dimension=e_c_size),
+    tf.feature_column.numeric_column(key="port"),
+    #tf.feature_column.indicator_column(categorical_column=loc_cul),
+    tf.feature_column.numeric_column(key="zip"),
+    #tf.feature_column.indicator_column(categorical_column=pro_cul),
+    tf.feature_column.numeric_column(key="times")
+]
+
+#n_class tell possible number of value model can predict it could be attack or not attack meaning two outcomes
+classifier = tf.estimator.DNNClassifier(model_dir=DIR,feature_columns=my_feature_columns,hidden_units=[100, 50],n_classes=2)
+#he ideal batch size depends on the problem. As a rule of thumb, smaller batch sizes usually enable the train method to train the model faster at the expense (sometimes) of accuracy
+#he default value of args.train_steps is 1000. The number of steps to train is a hyperparameter you can tune. Choosing the right number of steps usually requires both experience and experimentation.
+print (train_feature)
+print (test_label)
+classifier.train(input_fn=lambda:pipe.train_input_fn(train_feature, train_label,batch_size=100) ,steps=10)
+
+
+# Evaluate the model.
+
+eval_result = classifier.evaluate(input_fn=lambda:pipe.eval_input_fn(test_feature, test_label, batch_size=40))
+
+print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
---
+++ DataPipeline.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import math as math
+import argparse
+class DataPipeline:
+  '''
+  def train_input_fn(self,features, labels, batch_size):
+      """An input function for training"""
+      # Convert the inputs to a Dataset.
+      dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+      # Shuffle, repeat, and batch the examples.
+      dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+
+      # Return the dataset.
+      return dataset
+  '''
+  #Google Train input function
+  def eval_input_fn(self,features, labels=None, batch_size=None):
+    """An input function for evaluation or prediction"""
+    if labels is None:
+      # No labels, use only features.
+      inputs = features
+    else:
+      inputs = (features, labels)
+
+    # Convert inputs to a tf.dataset object.
+    dataset = tf.data.Dataset.from_tensor_slices(inputs)
+
+    # Batch the examples
+    assert batch_size is not None, "batch_size must not be None"
+    dataset = dataset.batch(batch_size)
+
+    # Return the read end of the pipeline.
+    return dataset.make_one_shot_iterator().get_next()
+
+
+  def train_input_fn(self,features, labels, batch_size):
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+    # Setting the buffer_size to a value larger than the number of examples (120) buffer size 1000 ensures that the data will be well shuffled.
+    #examples 1000 buffere size 10000
+    dataset = dataset.shuffle(buffer_size=10000).repeat(count=None).batch(batch_size)
+    return dataset.make_one_shot_iterator().get_next()
+
+  TRAIN_URL = "data_train.csv"
+  TEST_URL = "data_test.csv"
+
+  CSV_COLUMN_NAMES = ['ip','port','loc_country','zip','protocol','ts','times','is_attack']
+
+
+  def load_data(self,label_name='is_attack'):
+      """Parses the csv file in TRAIN_URL and TEST_URL."""
+
+      # Parse the local CSV file.
+      train = pd.read_csv(filepath_or_buffer=self.TRAIN_URL,
+                          names=self.CSV_COLUMN_NAMES,  # list of column names
+                          dtype={"ip": str,"loc_country":str,"protocol":str},
+                          header=0  # ignore the first row of the CSV file.
+                         )
+      # train now holds a pandas DataFrame, which is data structure
+      # analogous to a table.
+
+      # 1. Assign the DataFrame's labels (the right-most column) to train_label.
+      # 2. Delete (pop) the labels from the DataFrame.
+      # 3. Assign the remainder of the DataFrame to train_features
+
+      # poping ts because it's of no use maybe in future models
+      train.pop('ts')
+
+
+      train_features, train_label = train, train.pop(label_name)
+
+
+
+      # Apply the preceding logic to the test set.
+      test = pd.read_csv(self.TEST_URL, names=self.CSV_COLUMN_NAMES, header=0)
+      test.pop('ts')
+      test_features, test_label = test, test.pop(label_name)
+
+      # Return four DataFrames.
+      return (train_features, train_label), (test_features, test_label)
